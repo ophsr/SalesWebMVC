dade5634a6dda03e6aa676849e2df43b745df184
Pedro <phsr2001@gmail.com>
2020-08-13 17:13:22 -0300

    SellerFormViewModel and Department select component

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Models;
-using SalesWebMVC.Data;
+using SalesWebMVC.Models.ViewModel;
 using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
@@ -12,10 +12,12 @@ namespace SalesWebMVC.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -25,14 +27,15 @@ namespace SalesWebMVC.Controllers
         }
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
-            seller.Department = _context.Department.Firts();
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
---
+++ SalesWebMVC/Models/ViewModel/SellerFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+
+namespace SalesWebMVC.Models.ViewModel
+{
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+
+    }
+}
